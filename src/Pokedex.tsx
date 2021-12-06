a48fc65e9edb923667f7bb7d662506f16cf39c2a
Julia Undeutsch <JUndeutsch@gmx.at>
2021-12-06 18:57:04 +0100

    fetch pokedex

--- src/Pokedex.tsx
+++ src/Pokedex.tsx
@@ -1,5 +1,41 @@
+import { useEffect, useState } from 'react';
+
 const Pokedex = () => {
-  return <h1>Pokedex</h1>;
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    getPokemon();
+  }, []);
+
+  const getPokemon = async () => {
+    try {
+      const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=5');
+      const { results } = await res.json();
+      const pokedex = results.map((pokemon: any, index: number) => {
+        const paddedId = ('00' + (index + 1)).slice(-3);
+        const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedId}.png`;
+        return { ...pokemon, image };
+      });
+      setPokemon(pokedex);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      {pokemon.map((pokemon: any, index: number) => (
+        <li key={index}>
+          <a href={`/pokedex?id=${index + 1}`}>
+            <img src={pokemon.image} alt={pokemon.name} />
+            <p>
+              {index + 1}.<span>{pokemon.name}</span>
+            </p>
+          </a>
+        </li>
+      ))}
+    </div>
+  );
 };
 
 export default Pokedex;
