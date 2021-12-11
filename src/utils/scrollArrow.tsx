d0d3eaeac0beb025d24933f2f0c8245dff3fe9c6
Julia Undeutsch <JUndeutsch@gmx.at>
2021-12-11 13:16:03 +0100

    add useEffect to scroll button

--- src/utils/scrollArrow.tsx
+++ src/utils/scrollArrow.tsx
@@ -1,22 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ArrowUpCircleFill } from 'react-bootstrap-icons';
 
 export const ScrollArrow = () => {
   const [showScroll, setShowScroll] = useState(false);
 
-  const checkScrollTop = () => {
-    if (!showScroll && window.pageYOffset > 400) {
-      setShowScroll(true);
-    } else if (showScroll && window.pageYOffset <= 400) {
-      setShowScroll(false);
-    }
-  };
-
   const scrollTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
-  window.addEventListener('scroll', checkScrollTop);
+  useEffect(() => {
+    const checkScrollTop = () => {
+      if (!showScroll && window.pageYOffset > 400) {
+        setShowScroll(true);
+      } else if (showScroll && window.pageYOffset <= 400) {
+        setShowScroll(false);
+      }
+    };
+    window.addEventListener('scroll', checkScrollTop);
+    return () => window.removeEventListener('scroll', checkScrollTop);
+  }, [showScroll]);
 
   return (
     <ArrowUpCircleFill
