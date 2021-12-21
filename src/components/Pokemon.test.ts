87ccf7cc26ad1b69d7df00f091317c06b950a609
Julia Undeutsch <JUndeutsch@gmx.at>
2021-12-21 21:31:34 +0100

    create e2e tests for components

---
+++ src/components/Pokemon.test.ts
@@ -0,0 +1,10 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import scrollArrow from '../utils/scrollArrow';
+
+test('should render stats from clicked pokemon', () => {});
+
+test('should show evolutions of clicked pokemon', () => {});
+
+test('should change pokemon to clicked pokemon in evolutions', () => {});
