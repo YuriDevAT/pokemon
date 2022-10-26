3a35ff2e9d368c65519f2f319637b103d7cda476
Julia Undeutsch <JUndeutsch@gmx.at>
2022-10-26 11:45:58 +0100

    successfully wrote my first tests for the pokedex component

--- e2e/pokedex.spec.ts
+++ e2e/pokedex.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('pokedex has bulbasaur in list and go to details link linking to bulbasaurs details', async ({ page }) => {
-  // await page.goto('/');
+  await page.goto('http://localhost:3000/');
 
   // Expect a title "to contain" a substring.
-  // await expect(page).toHaveText(/bulbasaur/);
+  await expect(page).toHaveTitle(/Pokemon/);
 
   // create a locator
   const getDetails = page.getByText('bulbasaur');
@@ -12,9 +12,10 @@ test('pokedex has bulbasaur in list and go to details link linking to bulbasaurs
   // Expect an attribute "to be strictly equal" to the value.
   await expect(getDetails).toHaveAttribute('href', '/pokemon/1');
 
-  // Click the get started link.
+  // Click the get details link.
   await getDetails.click();
 
   // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.:id/);
+  await expect(page).toHaveURL(/.*1/);
 });
+
